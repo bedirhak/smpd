640a2e37bef521b9a04d174690b662c39c793d0d
Bedirhan KARA <bedirhank.bk41@gmail.com>
2023-05-22 09:08:15 +0300

    get report screen created

--- src/views/GetReport.js
+++ src/views/GetReport.js
@@ -2,21 +2,14 @@
 import { useLocation } from 'react-router-dom';
 import { useEffect, useState, useRef } from 'react';
 import firebase from 'firebase/compat/app';
-import '../style/addTreatment.css';
-
-import { doc, setDoc } from "firebase/firestore";
+import '../style/getReport.css';
 import {db, prescriptionsRef} from '../firebase';
+import { Link } from 'react-router-dom';
+
 
 const GetReport = (props) => {
-    const usagePeriodRef = useRef();
-    const [drugType, setDrugType] = useState('');
-    const [illness, setIllness] = useState('');
-    const [pillName, setPillName] = useState('');
-    const [startDate, setStartDate] = useState();
-    const [startTime, setStartTime] = useState();
-    const [totalPill, setTotalPill] = useState('');
-    const [usagePediodHour, setUsagePeriodHour] = useState();
-    const [medicationHours, setMedicationHours] = useState([]);
+  
+    const [waitingTreatments, setWaitingTreatments] = useState([]);
     const location = useLocation();
     const usersCollection = db.collection("users");
     const [userId, setUserId] = useState();
@@ -24,6 +17,7 @@ const GetReport = (props) => {
 
     useEffect(() => {
       console.log('user', location.state);
+      const waitingPrescriptions = 
 
         usersCollection
         .where("Mail", "==", location.state.Mail).get()
@@ -36,78 +30,96 @@ const GetReport = (props) => {
 
     });
 
-    const setUsagePeriodType = (usagePeriod) => {
-      setUsagePeriodHour(usagePeriod);
-      const selectElement = document.getElementById("smpd-medication-hours");
-      selectElement.selectedIndex = 0;
-      setMedicationHours([]);
-
-    }
-
-    const setMedicationHoursType = (usageHours) => {
-      const medicationHoursStringArray = usageHours.split(',');
-      const medicationHoursIntArray = medicationHoursStringArray.map((str) => {
-        return parseInt(str, 10);
-      });
-      setUsagePeriodHour(null);
-      setMedicationHours(medicationHoursIntArray);
-    }
-
-    const updateTime = (event) => {
-      const timeArray = event.target.value.split(":");
-      const selectedTime = timeArray[0] + ":00";
-
-      setStartTime(Number.parseInt(timeArray[0]));
-      event.target.value = selectedTime;
-    }
-
-    const handleSubmit = () => {
-
-    const dateObject = new Date(startDate);
-    console.log(startTime)
-    dateObject.setHours(startTime);
-    console.log('usage period hour', typeof usagePediodHour)
-
-      prescriptionsRef.add({
-        DrugType: drugType,
-        Illness: illness,
-        MedicationHours: medicationHours,
-        Name: pillName,
-        StartDate: firebase.firestore.Timestamp.fromDate(dateObject),
-        TotalPill: totalPill,
-        UsagePeriod: [],
-        UsagePeriodHour: usagePediodHour
-      })
-      .then((docRef) => {
-        console.log('Yeni belge eklendi. Belge kimliği:', docRef.id);
-        updateUser(docRef.id);
-      })
-      .catch((error) => {
-        console.error('Belge eklenirken hata oluştu:', error);
-      });
-
-    
-
-    }
-
-    const updateUser = (prescriptionId) => {
-      const userRef = db.collection("users").doc(userId);
-      const waitingTreatmentList = user.WaitingTreatmentList;
-      waitingTreatmentList.push(prescriptionId);
-
-
-      userRef.update({
-        WaitingTreatmentList: waitingTreatmentList,
-        NewTreatment: true,
-        MedicationHours: medicationHours
-      })
-    }
-
 
   return (
     <div className="smpd-clear-window">
       <h1 className='smpd-page-heading'>{user.Name } {user.Surname } Tedavi Raporu </h1>
       
+      <div className='smpd-treatments'>
+        <div className='smpd-compleated-treatments'>
+          {user.CompletedPrescriptionList.length > 0 &&
+          <>
+          <h4 className="smpd-treatments-type-heading">Tamamlanan Tedaviler</h4>
+          <table className="smpd-treatments-table">
+            <thead>
+              <th>İlaç Tipi</th>
+              <th>İlaç Adı</th>
+              <th>Şikayet</th>
+              <th>Kullanılan İlaç Adedi</th>
+              <th>Tedavi Detayı</th>
+            </thead>
+            <tbody>
+            {user && user.CompletedPrescriptionList.map((treatment, index) => (
+                <tr key={index}>
+                    <td>{treatment.DrugType}</td>
+                    <td>{treatment.Name}</td>
+                    <td>{treatment.Illness}</td>
+                    <td>{treatment.TotalPill}</td>
+                    <td className='smpd-table-buttons'><Link to= '/get-report' state={ treatment }>Tedavi Detayı</Link></td>
+                </tr>
+              ))  
+            }            
+            </tbody>
+          </table>
+          </>
+          }
+
+          {user.PrescriptionList.length > 0 &&
+          <>
+          <h4 className="smpd-treatments-type-heading">Aktif Tedaviler</h4>
+          <table className="smpd-treatments-table">
+            <thead>
+              <th>İlaç Tipi</th>
+              <th>İlaç Adı</th>
+              <th>Şikayet</th>
+              <th>Kullanılan İlaç Adedi</th>
+              <th>Tedavi Detayı</th>
+            </thead>
+            <tbody>
+            {user && user.PrescriptionList.map((treatment, index) => (
+                <tr key={index}>
+                    <td>{treatment.DrugType}</td>
+                    <td>{treatment.Name}</td>
+                    <td>{treatment.Illness}</td>
+                    <td>{treatment.TotalPill}</td>
+                    <td className='smpd-table-buttons'><Link to= '/get-report' state={ treatment }>Tedavi Detayı</Link></td>
+                </tr>
+              ))  
+            }            
+            </tbody>
+          </table>
+          </>
+          } 
+
+          {user.WaitingTreatmentList.length > 0 &&
+          <>
+          <h4 className="smpd-treatments-type-heading">Onay Bekleyen Tedaviler</h4>
+          <table className="smpd-treatments-table">
+            <thead>
+              <th>İlaç Tipi</th>
+              <th>İlaç Adı</th>
+              <th>Şikayet</th>
+              <th>Kullanılan İlaç Adedi</th>
+              <th>Tedavi Detayı</th>
+            </thead>
+            <tbody>
+            {user && user.WaitingTreatmentList.map((treatment, index) => (
+                <tr key={index}>
+                    <td>{treatment.DrugType}</td>
+                    <td>{treatment.Name}</td>
+                    <td>{treatment.Illness}</td>
+                    <td>{treatment.TotalPill}</td>
+                    <td className='smpd-table-buttons'><Link to= '/get-report' state={ treatment }>Rapor Al</Link></td>
+                </tr>
+              ))  
+            }            
+            </tbody>
+          </table>
+        </>
+        }
+        </div>
+      </div>
+
     </div>
   )
 }
